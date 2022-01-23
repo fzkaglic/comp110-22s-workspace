afaccd0ce7e0859357bdda4b451fda99989fbb57
Feifei Kaglic <fkaglic@unc.edu>
2022-01-22 21:20:13 -0500

    Exercise 1 Part 2

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -7,3 +7,15 @@ single_character: str = input("Enter a single character: ")
 print("Enter a 5-character word: ")
 print("Enter a single character: ")
 print("Searching for " + single_character + " in " + five_character_word)
+
+if single_character == five_character_word[0]:
+    print(single_character + " found at index 0")
+if single_character == five_character_word[1]:
+    print(single_character + " found at index 1")
+if single_character == five_character_word[2]:
+    print(single_character + " found at index 2")
+if single_character == five_character_word[3]:
+    print(single_character + " found at index 3")
+if single_character == five_character_word[4]:
+    print(single_character + " found at index 4")
+    
