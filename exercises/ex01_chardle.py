49e49f596e699eafdf7e3020edd5e97becfa5157
Feifei Kaglic <fkaglic@unc.edu>
2022-01-22 20:15:25 -0500

    Exercise 1 Part 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,8 +2,8 @@
 
 __author__ = "730501608"
 
-five_character_word: str = input("Enter a 5-character word:")
-single_character: str = input("Enter a single character:")
-print("Enter a 5-character word:")
-print("Enter a single character:")
+five_character_word: str = input("Enter a 5-character word: ")
+single_character: str = input("Enter a single character: ")
+print("Enter a 5-character word: ")
+print("Enter a single character: ")
 print("Searching for " + single_character + " in " + five_character_word)
