9635bf56085a247c1c4fb23b19b01d605209763f
Feifei Kaglic <fkaglic@unc.edu>
2022-01-23 16:39:48 -0500

    Exercise 1 Part 3

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -19,4 +19,18 @@ if single_character == five_character_word[3]:
 if single_character == five_character_word[4]:
     print(single_character + " found at index 4")
 
-frequency: int = 0
+frequency = int(five_character_word.count(single_character))
+
+if frequency == 0:
+    print("No instances of " + single_character + " found in " + five_character_word)
+else: 
+    if frequency == 1:
+        print("1 instance of " + single_character + " found in " + five_character_word)
+    if frequency == 2:
+        print("2 instances of " + single_character + " found in " + five_character_word)
+    if frequency == 3:
+        print("3 instances of " + single_character + " found in " + five_character_word)
+    if frequency == 4:
+        print("4 instances of " + single_character + " found in " + five_character_word)
+    if frequency == 5:
+        print("5 instances of " + single_character + " found in " + five_character_word)
