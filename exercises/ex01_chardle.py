4b417f3453919dcc22b2ea596f66bda73f95159b
Feifei Kaglic <fkaglic@unc.edu>
2022-01-22 20:09:33 -0500

    Exercise 1 Part 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,4 +1,10 @@
 """EX01 - Chardle - A cute step toward Wordle."""
 
-__author__= "730501608"
+__author__ = "730501608"
+
+five_character_word: str = input("Enter a 5-character word:")
+single_character: str = input("Enter a single character:")
+print("Enter a 5-character word:")
+print("Enter a single character:")
+print("Searching for " + single_character + " in " + five_character_word)
 
