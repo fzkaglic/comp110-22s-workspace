a89ad505519737a64e7916b41e5afd2f2c3b9a9a
Feifei Kaglic <fkaglic@unc.edu>
2022-01-31 14:57:16 -0500

    Exercise 2 Completed

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -12,11 +12,6 @@ while int(len(guess)) != int(len(secret_word)):
 # the while loop will continue prompting the user for input until
 # they enter a word with the correct letter length
 
-if guess == secret_word:
-    print("Woo! You got it!")
-else:
-    print("Not quite. Play again soon!")
-
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
@@ -28,7 +23,16 @@ while i < int(len(secret_word)):
     if guess[i] == secret_word[i]:
         emoji = emoji + GREEN_BOX
     else:
-        emoji = emoji + WHITE_BOX
+        contains_character: bool = False
+        alt_i: int = 0
+        while contains_character is not True and alt_i < int(len(secret_word)):
+            if secret_word[alt_i] == guess[i]:
+                contains_character = True
+            alt_i = alt_i + 1
+        if contains_character is True:
+            emoji = emoji + YELLOW_BOX
+        else: 
+            emoji = emoji + WHITE_BOX
     i = i + 1
 
 # the while loop tests whether the index of the guess
@@ -41,3 +45,8 @@ while i < int(len(secret_word)):
 # does not appear at all within the secret word
 
 print(emoji)
+
+if guess == secret_word:
+    print("Woo! You got it!")
+else:
+    print("Not quite. Play again soon!")
