c103811b2030c78cfd5ae71e514bbba93cdfa306
Feifei Kaglic <fkaglic@unc.edu>
2022-02-15 11:35:58 -0500

    Exercise 4 - Progress

--- exercises/ex04_turtle.py
+++ exercises/ex04_turtle.py
@@ -9,11 +9,10 @@ colormode(255)
 def main() -> None:
     """The entrypoint of my scene."""
     love: Turtle = Turtle()
-    joy: Turtle = Turtle()
+    cupid: Turtle = Turtle()
     rose(love, -250.0, -100.0, 30, 232, 51, 51, 134, 12, 12)
-    rose(love, -250.0, -100.0, 60, 251, 250, 79, 238, 152, 45)
-    heart(joy, -115.0, -50.0, 251, 250, 79, 238, 152, 45)
-    heart(joy, 0.0, -50.0, 251, 250, 79, 238, 152, 45)
+    rose(love, -220.0, 20.0, 50, 251, 250, 79, 238, 152, 45)
+    arrow(cupid, -230.0, 45.0, 251, 250, 79, 238, 152, 45)
     done()
 
 
@@ -33,15 +32,16 @@ def rose(love: Turtle, x: float, y: float, radius: int, p_red: int, p_green: int
     love.hideturtle()
 
 
-def heart(joy: Turtle, x: float, y: float, p_red: int, p_green: int, p_blue: int, f_red: int, f_green: int, f_blue: int) -> None:
-    """Draw a heart for Valentine's Day."""
-    joy.penup()
-    joy.goto(x, y)
-    joy.pendown()
-    joy.color((p_red, p_green, p_blue), (f_red, f_green, f_blue))
-    joy.begin_fill()
-    joy.shape("heart")
-    joy.hideturtle()
+def arrow(cupid: Turtle, x: float, y: float, p_red: int, p_green: int, p_blue: int, f_red: int, f_green: int, f_blue: int) -> None:
+    """Draw Cupid's arrows for Valentine's Day."""
+    cupid.penup()
+    cupid.goto(x, y)
+    cupid.pendown()
+    cupid.color((p_red, p_green, p_blue), (f_red, f_green, f_blue))
+    cupid.begin_fill()
+    cupid.shape("arrow")
+    cupid.end_fill()
+    cupid.hideturtle()
 
 
 if __name__ == "__main__":
