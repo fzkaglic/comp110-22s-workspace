ad50cfdf1d7096eb48eccb4af89ba8287d512007
Feifei Kaglic <fkaglic@unc.edu>
2022-02-15 12:07:23 -0500

    Exercise 4 - Getting Closer

--- exercises/ex04_turtle.py
+++ exercises/ex04_turtle.py
@@ -9,10 +9,10 @@ colormode(255)
 def main() -> None:
     """The entrypoint of my scene."""
     love: Turtle = Turtle()
-    cupid: Turtle = Turtle()
     rose(love, -250.0, -100.0, 30, 232, 51, 51, 134, 12, 12)
     rose(love, -220.0, 20.0, 50, 251, 250, 79, 238, 152, 45)
-    arrow(cupid, -230.0, 45.0, 251, 250, 79, 238, 152, 45)
+    card(love, -50.0, 45.0, 180, 31, 109, 255, 217, 237)
+    note(love, 100.0, -150.0, 195, 29, 53)
     done()
 
 
@@ -32,16 +32,32 @@ def rose(love: Turtle, x: float, y: float, radius: int, p_red: int, p_green: int
     love.hideturtle()
 
 
-def arrow(cupid: Turtle, x: float, y: float, p_red: int, p_green: int, p_blue: int, f_red: int, f_green: int, f_blue: int) -> None:
-    """Draw Cupid's arrows for Valentine's Day."""
-    cupid.penup()
-    cupid.goto(x, y)
-    cupid.pendown()
-    cupid.color((p_red, p_green, p_blue), (f_red, f_green, f_blue))
-    cupid.begin_fill()
-    cupid.shape("arrow")
-    cupid.end_fill()
-    cupid.hideturtle()
+def card(love: Turtle, x: float, y: float, p_red: int, p_green: int, p_blue: int, f_red: int, f_green: int, f_blue: int) -> None:
+    """Draw a card for Valentine's Day."""
+    love.penup()
+    love.goto(x, y)
+    love.setheading(0.0)
+    love.pendown()
+    love.color((p_red, p_green, p_blue), (f_red, f_green, f_blue))
+    love.begin_fill()
+    i: int = 0
+    while i < 4:
+        love.forward(300)
+        love.right(90)
+        i = i + 1
+    love.end_fill()
+    love.hideturtle()
+
+
+def note(love: Turtle, x: float, y: float, p_red: int, p_green: int, p_blue: int) -> None:
+    """Write a message for the Valentine's Day card."""
+    love.penup()
+    love.goto(x, y)
+    love.setheading(0.0)
+    love.pendown()
+    love.pencolor(p_red, p_green, p_blue)
+    love.write("Happy Valentine's Day, \nCOMP 110 Team!\n", False, align="center", font=('Times New Roman', 18, 'italic'))
+    love.hideturtle()
 
 
 if __name__ == "__main__":
